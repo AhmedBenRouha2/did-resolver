e62842b8c48b23e8753d1b4e7f05dad140f526a2
ahmed <ahmed.benrouha@esprit.tn>
2020-07-02 12:00:41 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Services = require('./services/did-resolver.service');
 
-module.exports = function DidResolver(provider, ABI) {
-    this.wrapDIdToDocument = (DID) => {
+module.exports = {
+    provider = "",
+    ABI = "",
+
+    DidResover= (provider, ABI) => {
+        provider = provider;
+        ABI = ABI;
+    },
+
+    wrapDIdToDocument = (DID) => {
         let services = new Services(provider, ABI);
         let did = DID;
         let contractAddress = "0x" + did.split(':')[2];
@@ -16,54 +24,54 @@ module.exports = function DidResolver(provider, ABI) {
     },
 
 
-        this.addDelegate = (DATA) => {
-            let services = new Services(provider, ABI);
-            let data = DATA;
+    addDelegate = (DATA) => {
+        let services = new Services(provider, ABI);
+        let data = DATA;
+        let contractAddress = data.contractAddress
+        let delegateAddress = data.delegateAddress
+        let validTo = data.delegateAddress
+        let citizenAddress = data.citizenAddress
+        return new Promise((resolve, reject) => {
+            services.unlockAccount(citizenAddress).then(result => {
+                services.addDelegate(delegateAddress, validTo, contractAddress, citizenAddress).then(result => {
+                    resolve(result);
+                })
+            })
+        });
+    },
+
+    manageAttribute = (_Action, _Data) => {
+        let services = new Services(provider, ABI);
+        let action = _Action;
+        if (action === 'add' || action === 'revoke') {
+            let data = _Data;
             let contractAddress = data.contractAddress
-            let delegateAddress = data.delegateAddress
-            let validTo = data.delegateAddress
+            let attributeType = data.attributeType
+            let value = data.value
             let citizenAddress = data.citizenAddress
+            let valideTo = 0;
+            if (action === 'add') {
+                valideTo = data.valideTo
+            }
             return new Promise((resolve, reject) => {
                 services.unlockAccount(citizenAddress).then(result => {
-                    services.addDelegate(delegateAddress, validTo, contractAddress, citizenAddress).then(result => {
+                    services.addAttribute(attributeType, valideTo, value, contractAddress, citizenAddress).then(result => {
                         resolve(result);
                     })
-                })
-            });
-        },
-
-        this.manageAttribute = (_Action, _Data) => {
-            let services = new Services(provider, ABI);
-            let action = _Action;
-            if (action === 'add' || action === 'revoke') {
-                let data = _Data;
-                let contractAddress = data.contractAddress
-                let attributeType = data.attributeType
-                let value = data.value
-                let citizenAddress = data.citizenAddress
-                let valideTo = 0;
-                if (action === 'add') {
-                    valideTo = data.valideTo
-                }
-                return new Promise((resolve, reject) => {
-                    services.unlockAccount(citizenAddress).then(result => {
-                        services.addAttribute(attributeType, valideTo, value, contractAddress, citizenAddress).then(result => {
-                            resolve(result);
-                        })
-                    });
                 });
-            }
-        },
-
-        this.testAsync = (data) => {
-            return new Promise((resolve, reject) => {
-
-                resolve(data);
-
             });
-        },
-        this.testSync = (data) => {
-            return(data);
-
         }
+    },
+
+    testAsync: (data) => {
+        return new Promise((resolve, reject) => {
+
+            resolve(data);
+
+        });
+    },
+    testSync: (data) => {
+        return (data);
+
+    }
 }
